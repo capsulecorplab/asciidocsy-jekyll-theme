29b445882c3f950f680f14d5e9ce5833e6953737
capsulecorplab <capsulecorplab@users.noreply.github.com>
2021-07-16 13:31:01 +0000

    deploy: c07b56948ef31ade0def88263208396a6e719e53

--- buildDocs.sh
+++ buildDocs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm --volume="$PWD:/src" -w "/src" capsulecorplab/asciidoctor-extended:asciidocsy 'jekyll build'
+docker run --rm --volume="$PWD:/src" -w "/src" capsulecorplab/asciidoctor-extended:asciidocsy 'rm Gemfile.lock && jekyll build'
--- serveDocs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker run --rm --volume $PWD:/src -w /src -p 4000:4000 capsulecorplab/asciidoctor-extended:asciidocsy 'jekyll serve --host 0.0.0.0'
-
